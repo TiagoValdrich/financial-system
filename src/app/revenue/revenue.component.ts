b6f08b57ecc92c526aa5a9b8bd8f14679084ba28
Tiago Valdrich <tiago.valdrich@camerite.com>
2019-11-17 13:41:36 -0300

    Creating revenues crud page.

---
+++ src/app/revenue/revenue.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { RevenueService } from '../services/revenue.service';
+import { ToastrService } from 'ngx-toastr';
+
+
+@Component({
+  selector: 'app-revenue',
+  templateUrl: './revenue.component.html',
+  styleUrls: ['./revenue.component.scss']
+})
+export class RevenueComponent implements OnInit {
+
+  public revenues;
+
+  constructor(
+    private revenueService: RevenueService,
+    private toastrService: ToastrService
+  ) { }
+
+  ngOnInit() {
+    this.revenueService
+      .getRevenues()
+      .then((revenues: any) => {
+        this.revenues = revenues;
+      })
+      .catch((err) => {
+        this.toastrService.error('Erro ao buscar receitas');
+        console.error('Failed to load revenues', err);
+      });
+  }
+
+  public deleteRevenue(id: number): void {
+    this.revenueService.deleteRevenue(id)
+      .then(() => {
+        this.revenues = this.revenues.filter(revenue => {
+          return revenue.id !== id;
+        });
+        this.toastrService.success('Receita excluida com sucesso!');
+      })
+      .catch((err) => {
+        this.toastrService.error('Erro ao excluir receita!');
+        console.error('Error to delete revenue', err);
+      });
+  }
+
+}
